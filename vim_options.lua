c21e0478c1d90fd427dc1ec5d38154ca1965c26b
Nitesh Shetty <nitheshshetty@gmail.com>
2024-02-23 15:50:11 +0530

    vim options: fix the options were not loading issue

--- vim_options.lua
+++ vim_options.lua
@@ -1,42 +1,40 @@
 -- vim options 
-return {
 
-	config = function()
-	-- Set highlight on search
-	vim.o.hlsearch = false
+-- Set highlight on search
+vim.o.hlsearch = false
 
-	-- Set relativenumber
-	vim.o.relativenumber = true
+-- Set relativenumber
+vim.o.relativenumber = true
 
-	-- Set colorcolumn
-	-- vim.o.colorcolumn = "76,81"
-	vim.opt.colorcolumn = "76,81"
+-- Set colorcolumn
+-- vim.o.colorcolumn = "76,81"
+vim.opt.colorcolumn = "76,81"
 
-	-- Make line numbers default
-	vim.wo.number = true
+-- Make line numbers default
+vim.wo.number = true
 
-	-- Enable mouse mode
-	vim.o.mouse = 'a'
+-- Enable mouse mode
+vim.o.mouse = 'a'
 
-	-- Enable break indent
-	vim.o.breakindent = true
+-- Enable break indent
+vim.o.breakindent = true
 
-	-- Save undo history
-	vim.o.undofile = true
+-- Save undo history
+vim.o.undofile = true
 
-	-- Case insensitive searching UNLESS /C or capital in search
-	vim.o.ignorecase = true
-	vim.o.smartcase = true
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-	-- Decrease update time
-	vim.o.updatetime = 250
-	vim.wo.signcolumn = 'yes'
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
 
-	-- Set colorscheme
-	vim.o.termguicolors = true
-	vim.cmd [[colorscheme onedark]]
+-- Set colorscheme
+vim.o.termguicolors = true
+-- vim.cmd [[colorscheme onedark]]
 
-	-- Set completeopt to have a better completion experience
-	vim.o.completeopt = 'menuone,noselect'
-	end,
-}
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+return {}
